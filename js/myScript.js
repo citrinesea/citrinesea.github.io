9c037b955dc708aa8a21c13dab971f0055d229fa
citrinesea <watervyper@gmail.com>
2023-11-16 13:48:53 -0500

    Added fade-in-right

--- js/myScript.js
+++ js/myScript.js
@@ -14,3 +14,29 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+const elementsToFadeInUpOnScroll = document.querySelectorAll(".fade-in-up-on-scroll");
+if (elementsToFadeInUpOnScroll) {
+  window.addEventListener("scroll", function(event) {
+    elementsToFadeInUpOnScroll.forEach(function(element) {
+      if (window.scrollY >= (element.offsetTop - window.innerHeight)) {
+        element.classList.add("fade-in-up");
+      } else {
+        element.classList.remove("fade-in-up");
+      }
+    });
+  });
+}
+
+const elementsToFadeInOnLoad = document.querySelectorAll(".fade-in-on-load");
+if (elementsToFadeInOnLoad) {
+  window.addEventListener("load", function(event) {
+    elementsToFadeInOnLoad.forEach(function(element) {
+      if (window.scrollY >= (element.offsetTop - window.innerHeight)) {
+        element.classList.add("fade-in");
+      } else {
+        element.classList.remove("fade-in");
+      }
+    });
+  });
+}
